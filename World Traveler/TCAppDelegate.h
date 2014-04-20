10b09ba5bf06400a61b591b5e64331964faf6da5
Humza <humzai@hotmail.com>
2014-04-19 19:16:05 -0700

    Initial Commit

---
+++ World Traveler/TCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TCAppDelegate.h
+//  World Traveler
+//
+//  Created by Humza Iqbal on 4/19/14.
+//  Copyright (c) 2014 Humza Iqbal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ World Traveler/TCViewController.m
@@ -0,0 +1,29 @@
+//
+//  TCViewController.m
+//  World Traveler
+//
+//  Created by Humza Iqbal on 4/19/14.
+//  Copyright (c) 2014 Humza Iqbal. All rights reserved.
+//
+
+#import "TCViewController.h"
+
+@interface TCViewController ()
+
+@end
+
+@implementation TCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
